52ee79708c816b8043ab74fbfe93b9e8690b1962
ViharevN <swix_off@mail.ru>
2022-10-21 01:59:39 +0300

    Строки. Домашнее задание. Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //строки домашнее задание
+        //задание 1
+
+        String firstName = new String("Ivan");
+        String middleName = new String("Ivanovich");
+        String lastName = new String("Ivanov");
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — "+ fullName);
+
 
     }
 }
